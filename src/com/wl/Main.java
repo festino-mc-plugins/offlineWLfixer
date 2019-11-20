043be9b67faf1e83e7e5d6c2d6ebc034f26ce0b1
Festino <offestashka@mail.ru>
2019-11-20 20:39:05 +0300

    Renamed from mainListener, removed command-handling code

---
+++ src/com/wl/Main.java
@@ -0,0 +1,26 @@
+package com.wl;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	static final String COMMAND_WLF = "wl";
+
+	public void onEnable()
+	{
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new Client(), this);
+
+		WLCommandExecutor ce = new WLCommandExecutor();
+		WLTabCompleter tc = new WLTabCompleter();
+    	getCommand(COMMAND_WLF).setExecutor(ce);
+    	getCommand(COMMAND_WLF).setTabCompleter(tc);
+	}
+}

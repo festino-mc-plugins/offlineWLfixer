218fa9000e43cbbd81374b716691900aa2c04b19
Festino <offestashka@mail.ru>
2020-03-08 08:22:05 +0300

    Removed unnecessary imports

--- src/com/wl/Main.java
+++ src/com/wl/Main.java
@@ -1,12 +1,5 @@
 package com.wl;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
